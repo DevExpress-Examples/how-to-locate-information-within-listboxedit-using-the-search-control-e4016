ff35bb96aa484cbdb5ac048d5b0668fa39b735cc
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ListBoxFilteringUsingSearchPanel/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Controls;
+
+namespace ListBoxFilteringUsingSearchPanel {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            listBox.ItemsSource = Stuff.GetStuff();
+        }
+    }
+}
---
+++ VB/ListBoxFilteringUsingSearchPanel/MainPage.xaml.vb
@@ -0,0 +1,12 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows.Controls
+
+Namespace ListBoxFilteringUsingSearchPanel
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+			listBox.ItemsSource = Stuff.GetStuff()
+		End Sub
+	End Class
+End Namespace
